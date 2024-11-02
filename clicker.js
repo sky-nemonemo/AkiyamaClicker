12be6f95a64b99b4cfc91720bc5995d4d8759396
sky-nemonemo <nobady928@yahoo.co.jp>
2024-11-03 01:16:14 +0900

    Add save and load

--- clicker.js
+++ clicker.js
@@ -3,15 +3,28 @@ var data = {
 };
 
 function init() {
+    loadData();
     updateCounter();
-    $(".akiyama").on("click", function(event) {
+    $(".akiyama").on("click", function (event) {
         data.count++;
+        saveData();
         updateCounter();
     });
 }
 
 function updateCounter() {
     $(".counter").html("<font color='#F9629F'>❤</font> " + data.count);
+}
+
+function saveData() {
+    document.cookie = "clickerData=" + btoa(JSON.stringify(data)) + "; max-age=31536000";
+}
+
+function loadData() {
+    const cookieValue = document.cookie.split("; ").find((row) => row.startsWith("clickerData="))?.split("=")[1];
+    if (cookieValue != null && cookieValue != "") {
+        data = JSON.parse(atob(cookieValue));
+    }
 }
 
 window.addEventListener('load', init);
